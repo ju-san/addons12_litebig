fc3c2e212e94d1569b522026900cc2c846625ce0
Ade <adeanshori@gmail.com>
2019-12-13 15:07:47 +0700

    update

--- aos_nasa_purchase/models/purchase.py
+++ aos_nasa_purchase/models/purchase.py
@@ -209,31 +209,45 @@ class PurchaseOrderLine(models.Model):
         if not self.product_id:
             return
         params = {'order_id': self.order_id}
-        seller = self.product_id._select_seller(
-            partner_id=self.partner_id,
-            quantity=self.product_qty,
-            date=self.order_id.date_order and self.order_id.date_order.date(),
-            uom_id=self.product_uom,
-            params=params)
+        
+        vals = {}
+        domain = {'product_uom': [('category_id', '=', self.product_id.uom_id.category_id.id)]}
+        if not self.product_uom or (self.product_id.uom_id.id != self.product_uom.id):
+            vals['product_uom'] = self.product_id.uom_id
+            vals['product_qty'] = self.product_uom_qty or 1.0
+        product = self.product_id.with_context(
+            lang=self.order_id.partner_id.lang,
+            partner=self.order_id.partner_id,
+            quantity=vals.get('product_qty') or self.product_qty,
+            date=self.order_id.date_order,
+            pricelist=self.order_id.pricelist_id.id,
+            uom=self.product_uom.id
+        )
+#         seller = self.product_id._select_seller(
+#             partner_id=self.partner_id,
+#             quantity=self.product_qty,
+#             date=self.order_id.date_order and self.order_id.date_order.date(),
+#             uom_id=self.product_uom,
+#             params=params)
 
-        if seller or not self.date_planned:
-            self.date_planned = self._get_date_planned(seller).strftime(DEFAULT_SERVER_DATETIME_FORMAT)
-
-        if not seller:
-            if self.product_id.seller_ids.filtered(lambda s: s.name.id == self.partner_id.id):
-                self.price_unit = 0.0
-            return
+#         if seller or not self.date_planned:
+#             self.date_planned = self._get_date_planned(seller).strftime(DEFAULT_SERVER_DATETIME_FORMAT)
+# 
+#         if not seller:
+#             if self.product_id.seller_ids.filtered(lambda s: s.name.id == self.partner_id.id):
+#                 self.price_unit = 0.0
+#             return
         
 
-        price_unit = self.env['account.tax']._fix_tax_included_price_company(seller.price, self.product_id.supplier_taxes_id, self.taxes_id, self.company_id) if seller else 0.0
+        price_unit = self.env['account.tax']._fix_tax_included_price_company(self._get_display_price(product), self.product_id.supplier_taxes_id, self.taxes_id, self.company_id)# if seller else 0.0
         if self.order_id.pricelist_id and self.order_id.partner_id:
-            price_unit = self.env['account.tax']._fix_tax_included_price_company(self._get_display_price(self.product_id), self.product_id.supplier_taxes_id, self.taxes_id, self.company_id) if seller else 0.0
-        if price_unit and seller and self.order_id.currency_id and seller.currency_id != self.order_id.currency_id:
-            price_unit = seller.currency_id._convert(
-                price_unit, self.order_id.currency_id, self.order_id.company_id, self.date_order or fields.Date.today())
+            price_unit = self.env['account.tax']._fix_tax_included_price_company(self._get_display_price(self.product_id), self.product_id.supplier_taxes_id, self.taxes_id, self.company_id)# if seller else 0.0
+#         if price_unit and seller and self.order_id.currency_id and seller.currency_id != self.order_id.currency_id:
+#             price_unit = seller.currency_id._convert(
+#                 price_unit, self.order_id.currency_id, self.order_id.company_id, self.date_order or fields.Date.today())
 
-        if seller and self.product_uom and seller.product_uom != self.product_uom:
-            price_unit = seller.product_uom._compute_price(price_unit, self.product_uom)
+#         if seller and self.product_uom and seller.product_uom != self.product_uom:
+#             price_unit = seller.product_uom._compute_price(price_unit, self.product_uom)
 
         self.price_unit = price_unit
     
