4003b6c1a0bfebd53e4f9ce8734a8ff7e6e59a39
Ade <adeanshori@gmail.com>
2020-01-07 15:33:36 +0700

    update

--- aos_nasa_purchase/models/stock.py
+++ aos_nasa_purchase/models/stock.py
@@ -11,6 +11,26 @@ from odoo.tools.misc import formatLang, format_date
 #     fold = fields.Boolean(string='Folded in Kanban',
 #         help='This stage is folded in the kanban view when there are no records in that stage to display.')
 
+class StockProductionLotGroup(models.Model):
+    _name = 'stock.production.lot.group'
+    _inherit = ['mail.thread','mail.activity.mixin']
+    _description = 'SN Group'
+    
+    name = fields.Char(
+        'Group Lot/Serial Number', default=lambda self: self.env['ir.sequence'].next_by_code('stock.lot.group.serial'),
+        required=True, help="Group Unique Lot/Serial Number")
+    ref = fields.Char('Internal Reference', help="Internal reference number in case it differs from the manufacturer's lot/serial number")
+    lot_ids = fields.One2many('stock.production.lot', 'group_id', 'Lots', readonly=True)
+    
+    _sql_constraints = [
+        ('name_ref_uniq', 'unique (name)', 'The group of serial number must be unique !'),
+    ]
+    
+class StockProductionLot(models.Model):
+    _inherit = 'stock.production.lot'
+    
+    group_id = fields.Many2one('stock.production.lot.group', string='Group SN')
+    
 class StockPicking(models.Model): 
     _inherit = 'stock.picking'
     
@@ -39,7 +59,9 @@ class StockMoveLine(models.Model):
             mline.group_id = mline.picking_id and mline.picking_id.group_id and mline.picking_id.group_id.name \
                 or mline.picking_id and mline.picking_id.origin or ''
             #mline.display_location_dest_id = mline.partner_id and mline.partner_id.name if mline.location_dest_id.usage in ('customer','supplier') else mline.location_dest_id.display_name
-        
+            
+    
+    group_lot_id = fields.Many2one('stock.production.lot.group', string='Group Lot/SN', related='lot_id.group_id')
     location_id = fields.Many2one('stock.location', 'Source', required=True)
     location_dest_id = fields.Many2one('stock.location', 'Destination', required=True)    
     display_location_id = fields.Char('From', compute='_compute_display_location', store=False)
@@ -48,21 +70,3 @@ class StockMoveLine(models.Model):
     #reference = fields.Char(string='Reference Warehouse', related='move_id.reference', store=True, related_sudo=False, readonly=False)
     
     
-class StockProductionLotGroup(models.Model):
-    _name = 'stock.production.lot.group'
-    _inherit = ['mail.thread','mail.activity.mixin']
-    _description = 'SN Group'
-    
-    name = fields.Char(
-        'Group Lot/Serial Number', default=lambda self: self.env['ir.sequence'].next_by_code('stock.lot.group.serial'),
-        required=True, help="Group Unique Lot/Serial Number")
-    ref = fields.Char('Internal Reference', help="Internal reference number in case it differs from the manufacturer's lot/serial number")
-    lot_ids = fields.One2many('stock.production.lot', 'group_id', 'Lots', readonly=True)
-    
-    _sql_constraints = [
-        ('name_ref_uniq', 'unique (name)', 'The group of serial number must be unique !'),
-    ]
-class StockProductionLot(models.Model):
-    _inherit = 'stock.production.lot'
-    
-    group_id = fields.Many2one('stock.production.lot.group', string='Group SN')
